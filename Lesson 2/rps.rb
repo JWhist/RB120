8f443d835c57752944407f7d6dfe97dc886fd560
Jordan Whistler <whistler.5@osu.edu>
2020-07-21 23:40:31 -0400

    rpsls version 2

--- Lesson 2/rps.rb
+++ Lesson 2/rps.rb
@@ -1,31 +1,17 @@
 class Move
-  VALUES = ['rock', 'paper', 'scissors']
+  VALUES = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+  attr_reader :value
+
   def initialize(value)
     @value = value
   end
 
-  def scissors?
-    @value == 'scissors'
+  def >(other)
+    win_against.include?(other.value)
   end
 
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
-  end
-
-  def <(other_move)
-    (rock? && other_move.paper?) ||
-      (paper? && other_move.scissors?) ||
-      (scissors? && other_move.rock?)
+  def <(other)
+    other.win_against.include?(value)
   end
 
   def to_s
@@ -33,66 +19,206 @@ class Move
   end
 end
 
-class RPSGame
-  attr_accessor :human, :computer
+class Rock < Move
+  attr_reader :win_against
 
   def initialize
+    super('Rock')
+    @win_against = ['Scissors', 'Lizard']
+  end
+end
+
+class Paper < Move
+  attr_reader :win_against
+
+  def initialize
+    super('Paper')
+    @win_against = ['Rock', 'Spock']
+  end
+end
+
+class Scissors < Move
+  attr_reader :win_against
+
+  def initialize
+    super('Scissors')
+    @win_against = ['Paper', 'Lizard']
+  end
+end
+
+class Lizard < Move
+  attr_reader :win_against
+
+  def initialize
+    super('Lizard')
+    @win_against = ['Spock', 'Paper']
+  end
+end
+
+class Spock < Move
+  attr_reader :win_against
+
+  def initialize
+    super('Spock')
+    @win_against = ['Scissors', 'Rock']
+  end
+end
+
+class RPSGame
+  MAX_SCORE = 5
+  attr_accessor :human, :computer
+
+  @@history = []
+
+  def initialize
+    system('clear') || system('cls')
+    display_welcome_message
     @human = Human.new
     @computer = Computer.new
   end
 
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
+    puts "\nWelcome to Rock, Paper, Scissors, Lizard & Spock!"
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors.  Good bye!"
+    puts "\nThanks for playing Rock, Paper, Scissors, Lizard & Spock! Goodbye!"
   end
 
   def display_choices
-    puts "#{human.name} chose #{human.move}"
+    puts "\n#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
   end
 
-  def display_winner
-    if human.move > computer.move
-      puts "#{human.name} won!"
-    elsif human.move < computer.move
-      puts "#{computer.name} won!"
+  def tension
+  puts('1..')
+  sleep(0.5)
+  puts('2..')
+  sleep(0.5)
+  puts('3...')
+  sleep(0.5)
+  puts 'SHOOT!'
+end
+
+  def human_wins?
+    human.move > computer.move
+  end
+
+  def computer_wins?
+    computer.move > human.move
+  end
+
+  def update_scores
+    if human_wins?
+      human.score += 1
+      @@history << "Win"
+    elsif computer_wins?
+      computer.score += 1
+      @@history << "Loss"
     else
-      puts "It's a tie!"
+      @@history << "Tie"
     end
+  end
+
+  def max_score_reached?
+    human.score == MAX_SCORE || computer.score == MAX_SCORE
+  end
+
+  def display_winner
+    puts "\n#{human.name} won!" if human_wins?
+    puts "\n#{computer.name} won!" if computer_wins?
+    puts "\nIt's a tie!" if !human_wins? && !computer_wins?
+  end
+
+  def display_scores
+    puts "\n#{human.name}: #{human.score}  #{computer.name}: #{computer.score}"
   end
 
   def play_again?
     answer = ''
     loop do
-      puts "Would you like to play again? (Y/N)"
-      answer = gets.chomp
-      break if ['y', 'Y', 'n', 'N'].include?(answer)
+      print "Would you like to play again? (Y/N) "
+      answer = gets.chomp.upcase
+      break if ['Y', 'N'].include?(answer)
       puts "Please enter Y or N"
     end
-    answer.upcase == 'Y'
+    answer == 'Y'
+  end
+
+  def main_loop
+    system('clear') || system('cls')
+    display_scores
+    human.choose
+    tension
+    computer.choose
+    display_choices
+    update_scores
+    display_winner
+    sleep(1.5)
   end
 
   def play
-    display_welcome_message
     loop do
-      human.choose
-      computer.choose
-      display_choices
-      display_winner
-      break unless play_again?
+      main_loop
+      break if max_score_reached?
     end
+    display_scores
     display_goodbye_message
+  end
+
+  def self.history
+    history_title_strip
+    @@history.each_slice(3) { |move| show_line(move) }
+  end
+
+  def self.move_history
+    @@history
+  end
+
+  def self.add_to_history(move)
+    @@history << move
+  end
+
+  class << self
+    def history_title_strip
+      puts "\nGAME HISTORY:"
+      puts "Player".ljust(10) + "Comp".ljust(10) + "P Move".ljust(10) +
+           "C Move".ljust(10) + "W/L".ljust(10)
+      puts "-" * 50
+    end
+
+    def short_names(move)
+      move[0][0][0..8].ljust(10) + move[1][0][0..8].ljust(10)
+    end
+
+    def show_line(move)
+      puts short_names(move) + move[0][1].ljust(10) + move[1][1].ljust(10) +
+           move[2].ljust(10)
+    end
   end
 end
 
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :score
 
   def initialize
     set_name
+    @score = 0
+  end
+
+  def move_history
+    RPSGame.move_history.select { |pair| pair[0] == name }
+  end
+
+  def history
+    moves = move_history
+    puts
+    puts "PLAYER HISTORY:"
+    puts "Player".ljust(10) + "Move".ljust(10)
+    puts "-" * 20
+    moves.each do |pair|
+      puts pair[0][0..8].ljust(10) + pair[1].ljust(10)
+    end
   end
 end
 
@@ -100,7 +226,7 @@ class Human < Player
   def set_name
     n = ''
     loop do
-      puts "What's your name?"
+      print "\nWhat's your name?  "
       n = gets.chomp
       break unless n.empty?
       puts "Sorry, must enter a value."
@@ -111,12 +237,14 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper or scissors:"
-      choice = gets.chomp
+      puts
+      print "Please choose rock, paper, scissors, lizard or spock:  "
+      choice = gets.chomp.capitalize
       break if Move::VALUES.include?(choice)
       puts "Sorry, invalid choice."
     end
-    self.move = Move.new(choice)
+    self.move = Object.const_get(choice).new
+    RPSGame.add_to_history([name, move.to_s])
   end
 end
 
@@ -126,8 +254,10 @@ class Computer < Player
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.sample)
+    self.move = Object.const_get(Move::VALUES.sample).new
+    RPSGame.add_to_history([name, move.to_s])
   end
 end
 
 RPSGame.new.play
+RPSGame.history
